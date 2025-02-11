2ccbb13b6e66da3ba8e88d1f5ec0686affbc6285
suhyeon lee <tngus0673@naver.com>
2025-02-11 19:42:08 +0900

    Step 2: 객체지향 프로그래밍 구현하기

--- Closure-OOP.playground/Pages/OOP.xcplaygroundpage/Contents.swift
+++ Closure-OOP.playground/Pages/OOP.xcplaygroundpage/Contents.swift
@@ -1,3 +1,38 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// Animal 클래스
+class Animal {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func makeSound() {
+        print("Animal - makeSound()")
+    }
+}
+
+// Dog 클래스
+class Dog: Animal {
+    override func makeSound() {
+        print("Bark!")
+    }
+}
+
+// Cat 클래스
+class Cat: Animal {
+    override func makeSound() {
+        print("Meow!")
+    }
+}
+
+// Dog 객체 생성
+let dog = Dog(name: "dog")
+dog.makeSound() // Bark!
+
+// Cat 객체 생성
+let cat = Cat(name: "cat")
+cat.makeSound() // Meow!
+
+
