5d25fbe29cbbd7384c5358b1914915c4f3caf4cd
suhyeon lee <tngus0673@naver.com>
2025-03-11 11:01:08 +0900

    Ch2. 야구게임 만들기: 주석 추가 및 반복문 조건 수정

--- BaseballGame/BaseballGame/BaseballGame.swift
+++ BaseballGame/BaseballGame/BaseballGame.swift
@@ -18,7 +18,6 @@ struct BaseballGame {
     // 안내 문구
     mutating func setGame(){
         while true {
-            print()
             print("환영합니다! 원하시는 번호를 입력해주세요")
             print("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
             
@@ -36,30 +35,35 @@ struct BaseballGame {
             case 2: // 게임 기록 보기
                 self.printTryCount()
             case 3: // 종료하기
+                print("<숫자 야구 게임을 종료합니다>")
                 return
             default: // 1, 2, 3을 제외한 값
                 print("올바른 숫자를 입력해주세요!")
             }
+            print()
         }
     }
     
     // 1. 게임 시작
     mutating func start() {
         var count = 1 // 시도 횟수
+        var flag = true // while 반복 flag
         self.answer = createAnswer() // 랜덤 정답 숫자 생성
         
         print(answer ?? "")
         print("< 게임을 시작합니다 >")
         
         // 정답일 때까지 반복
-        while let input = readLine() {
+        while flag {
             print("숫자를 입력하세요")
+            guard let input = readLine() else { return }
             
             if validationInput(input: input) { // 입력 값 검증
                 if checkAnswer(input: input) { // 정답 검증
                     print("정답입니다!")
+                    print()
                     self.tryCount.append(count) // 정답일 때 시도 횟수 기록
-                    break
+                    flag = false
                 } else { // 틀렸을 때
                     count += 1
                 }
@@ -146,21 +150,42 @@ struct BaseballGame {
         }
     }
     
-    /* 입력값 검사
-     1. 입력값이 세글자인지 확인
-     2. 숫자가 아닌 값 (compactMap: 옵셔널 값 없앰)
-     3. 숫자 0 확인 (filter)
-     4. 중복 처리 (Set)
+    //MARK: Lv3: 입력값 검사
+    /* 조건
+     1. 숫자가 아닌 값 확인 (백의 자리가 0인 String 값은 Int로 변환하면서 없어짐)
+        Ex) "048" -> 48
+     2. Int 값을 다시 String 배열로 변환하여 Set을 통해 중복값 삭제
+     3. 필터링된 입력값이 3글자인지 확인
     */
     func validationInput(input: String) -> Bool {
-        guard let intInput = Int(input) else { return false }
-        let filteredInput = Set(String(intInput).map{$0})
+        guard let intInput = Int(input) else { return false } // 1.
+        let filteredInput = Set(String(intInput).map{$0}) // 2.
         
-        if filteredInput.count == 3 {
+        if filteredInput.count == 3 {   // 3.
             return true
         } else { // 배열의 개수가 3개가 아니면 잘못된 값을 입력한 것
             print("올바르지 않은 입력 값입니다.")
             return false
         }
     }
+    
+    
+    //MARK: Lv2: 입력값 검사
+    /* 조건
+     1. 입력값이 세글자인지 확인
+     2. 숫자가 아닌 값 (compactMap: 옵셔널 값 없앰)
+     3. 숫자 0 확인 (filter)
+     4. 중복 처리 (Set)
+    */
+//    func validationInput(input: String) -> Bool {
+//        if input.count != 3 { return false } // 1.
+//        let filteredInput = Set(input.compactMap{Int(String($0))}.filter{$0 != 0}) // 2, 3, 4
+//
+//        if filteredInput.count == 3 {
+//            return true
+//        } else { // 배열의 개수가 3개가 아니면 잘못된 값을 입력한 것
+//            print("올바르지 않은 입력 값입니다.")
+//            return false
+//        }
+//    }
 }
